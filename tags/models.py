0f5dd806f9b0a8c3db7dd6609d01c203772cc933
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-07-27 18:56:35 +0000

    add content type

--- tags/models.py
+++ tags/models.py
@@ -4,5 +4,7 @@ from django.contrib.contenttypes.models import ContentType
 class Tag(models.Model):
     label = models.CharField(max_length=255)
 
-class TagggedItem(models.Model):
+class TaggedItem(models.Model):
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
