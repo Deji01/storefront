ea2127a680e79e5c19f16517049c9575ed3bc51f
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-07-27 18:42:10 +0000

    add orderitems class

--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from numpy import product
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
@@ -41,9 +42,15 @@ class Order(models.Model):
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
-    
+
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
