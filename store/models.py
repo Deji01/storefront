65c7ea6374ba8de36a52153cdf86c23d86e3d985
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-07-27 18:21:45 +0000

    add customer membership choice field

--- store/models.py
+++ store/models.py
@@ -8,8 +8,19 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
 
 class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+
+    MEMBRSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold')
+        ]
+
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBRSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
