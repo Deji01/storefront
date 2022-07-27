d00b8ad6d62087edf882172da0751ef992d5f7ea
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-07-27 19:01:58 +0000

    add liked item class

--- tags/models.py
+++ tags/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.auth.models import User
 
 class Tag(models.Model):
     label = models.CharField(max_length=255)
@@ -10,3 +11,11 @@ class TaggedItem(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
+
+class LikedItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
+
+
