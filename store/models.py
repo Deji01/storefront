d25d3beb712b750e26ae3dc27b2cfffebfd1ce55
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-07-27 18:46:14 +0000

    add promotion class

--- store/models.py
+++ store/models.py
@@ -4,6 +4,10 @@ from numpy import product
 class Collection(models.Model):
     title = models.CharField(max_length=255)
 
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
@@ -11,6 +15,7 @@ class Product(models.Model):
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
@@ -34,11 +39,13 @@ class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
     PAYMENT_STATUS_COMPLETE = 'C'
     PAYMENT_STATUS_FAILED = 'F'
+
     PAYMENT_STATUS_CHOICES = [
         (PAYMENT_STATUS_PENDING,'Pending'),
         (PAYMENT_STATUS_COMPLETE,'Complete'),
         (PAYMENT_STATUS_FAILED,'Failed'),
         ]
+
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
