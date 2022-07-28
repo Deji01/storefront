1f06d91aa8f0d61cf231fd8af9b634c6021ceb38
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-07-28 18:44:48 +0000

    create admin account

--- storefront/settings.py
+++ storefront/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'storefront',
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'store',
     'tags',
+    'likes'
 ]
 
 MIDDLEWARE = [
