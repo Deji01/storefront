d766227282d074f7994e25306422ffa8df786bb8
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-07-28 19:07:50 +0000

    add products to the admin page

--- store/admin.py
+++ store/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Collection)
+admin.site.register(models.Product)
--- store/models.py
+++ store/models.py
@@ -26,6 +26,12 @@ class Product(models.Model):
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
 
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering =['title']
+        
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
     MEMBERSHIP_SILVER = 'S'
