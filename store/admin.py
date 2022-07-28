955d04f1ff2791c7f40fe095b39a1960431b81a5
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-07-28 19:21:59 +0000

    add a computed column to the prodcuts

--- store/admin.py
+++ store/admin.py
@@ -3,9 +3,15 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title','unit_price']
+    list_display = ['title','unit_price', 'inventory_status']
     list_editable = ['unit_price']
     list_per_page = 10
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'Ok'    
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
@@ -13,5 +19,5 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['memebership']
     ordering = ['first_name', 'last_name']
     list_per_page = 10
-    
+
 admin.site.register(models.Collection)
