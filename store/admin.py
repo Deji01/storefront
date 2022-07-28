fc463ed06c1994bde12bd9fc2e087973e5083862
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-07-28 19:23:58 +0000

    selecting related objects

--- store/admin.py
+++ store/admin.py
@@ -3,9 +3,10 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title','unit_price', 'inventory_status']
+    list_display = ['title','unit_price', 'inventory_status', 'collection']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_select_related = ['collection']
 
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
