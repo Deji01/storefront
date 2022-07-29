d99943e00611eed8a1e22f7fd45be5eabc1b476d
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-07-29 17:35:22 +0000

    delete object

--- store/views.py
+++ store/views.py
@@ -5,34 +5,47 @@ from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 
+
 @api_view()
 def collection_detail(request, pk):
     return Response('ok')
+
 
 @api_view(['GET', 'POST'])
 def product_list(request):
     if request.method == 'GET':
         queryset = Product.objects.select_related('collection').all()
-        serializer = ProductSerializer(queryset, many=True, context={'request':request})
+        serializer = ProductSerializer(
+            queryset, many=True, context={'request': request})
         return Response(serializer.data)
     elif request.method == 'POST':
-        serializer =ProductSerializer(
+        serializer = ProductSerializer(
             data=request.data
         )
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET', 'PUT'])
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, id):
-    product = get_object_or_404(Product, pk=id) 
+    product = get_object_or_404(Product, pk=id)
     if request.method == 'GET':
         serializer = ProductSerializer(product)
         return Response(serializer.data)
     elif request.method == 'PUT':
         serializer = ProductSerializer(
-        product, data=request.data
+            product, data=request.data
         )
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        if product.orderitem_set.count() > 0:
+            return Response(
+                {
+                'error': 'Product cannot be deleted because it is associated with an order item.'
+                }, status=status.HTTP_405_METHOD_NOT_ALLOWED
+                )
+        product.delete()
+        return Response(status =status.HTTP_204_NO_CONTENT)
