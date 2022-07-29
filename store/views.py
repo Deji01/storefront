27025cbb714aa6651205640d1764a4be7447e850
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-07-29 18:00:44 +0000

    colection endpoint

--- store/views.py
+++ store/views.py
@@ -6,9 +6,30 @@ from .models import Collection, Product
 from .serializers import ProductSerializer, CollectionSerializer
 from django.db.models import Count
 
-@api_view()
+@api_view(['GET', 'PUT', 'DELETE'])
 def collection_detail(request, pk):
-    return Response('ok')
+    collection = get_object_or_404(
+        Collection.objects.annotate(products_count = Count('products')).all(), pk=pk
+        )
+    if request.method == 'GET':
+        serializer = CollectionSerializer(collection)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = CollectionSerializer(
+            collection, data=request.data
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        if collection.products.count() > 0:
+            return Response(
+                {
+                'error': 'Collection cannot be deleted because it includes one or more products.'
+                }, status=status.HTTP_405_METHOD_NOT_ALLOWED
+                )
+        collection.delete()
+        return Response(status =status.HTTP_204_NO_CONTENT)
 
 @api_view(['GET', 'POST'])
 def collection_list(request):
