32ac463331e3262f4b5386bc7db4268c985a6d92
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-07-29 16:20:43 +0000

    use string related field

--- store/serializers.py
+++ store/serializers.py
@@ -8,7 +8,7 @@ class ProductSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    collection = serializers.PrimaryKeyRelatedField(queryset=Collection.objects.all())
+    collection = serializers.StringRelatedField(queryset=Collection.objects.all())
 
     def calculate_tax(self, product:Product):
         return product.unit_price * Decimal(1.1)
