e7a58b9572eeb4428efa8b559dff807b49cc1bff
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-07-30 01:58:05 +0000

    further cleanup

--- store/serializers.py
+++ store/serializers.py
@@ -1,22 +1,25 @@
-from rest_framework import serializers
 from decimal import Decimal
+from .models import Product, Collection
+from rest_framework import serializers
 
-from store.models import Product, Collection
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title', 'products_count']
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
-    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    collection = serializers.HyperlinkRelatedField(
-        queryset=Collection.objects.all(),
-        view
-    )
+    products_count = serializers.IntegerField()
 
-    def calculate_tax(self, product:Product):
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description', 'slug', 'inventory', 'unit_price', 'price_with_tax', 'collection']
+
+    price_with_tax = serializers.SerializerMethodField(
+        method_name='calculate_tax')
+
+    def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
+
+   
--- store/views.py
+++ store/views.py
@@ -1,6 +1,5 @@
 from django.db.models.aggregates import Count
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
