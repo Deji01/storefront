fad2bc54a37ad039790db1c536f738aa446b497f
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-07-30 02:09:09 +0000

    product list class based view

--- store/urls.py
+++ store/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.product_list),
+    path('products/', views.ProductList.as_view()),
     path('products/<int:id>', views.product_detail),
     path('collections/<int:pk>/', views.collection_detail, name='collection_detail'),
     path('collections/', views.collection_list),
