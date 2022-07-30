f24760f92a254f85e926e95e848bf69ea89c8d6b
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-07-30 03:32:43 +0000

    filtering

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,10 @@
+from posixpath import basename
 from django.urls import path
 from rest_framework_nested import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register('products', views.ProductViewSet)
+router.register('products', views.ProductViewSet, basename='products')
 router.register('collections', views.CollectionViewSet)
 
 products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
