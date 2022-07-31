58f4e22b916b0c5634e4bbd68381a7af8e260b8f
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-07-31 19:23:27 +0000

    log in

--- storefront/settings.py
+++ storefront/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 import os
 
@@ -166,7 +166,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'AUTH_HEADER_TYPES' : ('JWT',)
+    'AUTH_HEADER_TYPES' : ('JWT',),
+    'ACCESS_TOKEN_LIFETIME' : timedelta(days=1),
 }
 
 DJOSER = {
