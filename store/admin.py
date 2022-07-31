b4190b69b7df3e646f1ba46d2b56c7d56ec3d4bd
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-07-31 15:25:34 +0000

    create user profiles

--- store/admin.py
+++ store/admin.py
@@ -49,8 +49,9 @@ class CustomerAdmin(admin.ModelAdmin):
     search_fields = ['first_name__istartswith', 'last_name__istartswith']
     list_display = ['first_name', 'last_name', 'membership']
     list_editable = ['membership']
+    list_select_related = ['user']
     list_per_page = 10
-    ordering = ['first_name', 'last_name']
+    ordering = ['user__first_name', 'user__last_name']
 
     @admin.display(ordering='orders_count')
     def orders(self, customer):
--- store/models.py
+++ store/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 from uuid import uuid4
 from django.core.validators import MinValueValidator
-
+from django.contrib import admin
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
@@ -38,7 +38,6 @@ class Product(models.Model):
     class Meta:
         ordering = ['title']
 
-
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
     MEMBERSHIP_SILVER = 'S'
@@ -49,20 +48,25 @@ class Customer(models.Model):
         (MEMBERSHIP_SILVER, 'Silver'),
         (MEMBERSHIP_GOLD, 'Gold')
     ]
-
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(
         max_length=1, choices=MEMBRSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    @admin.display(ordering='user__first_name')
+    def first_name(self):
+        return self.user.first_name
+
+    @admin.display(ordering='user__last_name')
+    def last_name(self):
+        return self.user.last_name
 
     def __str__(self) -> str:
-        return f'{self.first_name} {self.last_name}'
+        return f'{self.user.first_name} {self.user.last_name}'
 
     class Meta:
-        ordering = ['first_name', 'last_name']
+        ordering = ['user__first_name', 'user__last_name']
 
 
 class Order(models.Model):
