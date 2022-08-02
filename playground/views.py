8963f0bb796f37ba55f89605db961cb9505a2d52
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-08-02 01:48:17 +0000

    install redis and celery

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.core.mail import EmailMessage, BadHeaderError
 # Create your views here.
 def say_hello(request):
+    try:
+        message = EmailMessage('subject', 'message', 'info@dejibuy.com', ['yo@dejibuy.com'])
+        message.send()
+    except BadHeaderError:
+        pass
     return render(request, 'hello.html', {'name': 'Ayodeji'})
