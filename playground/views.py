e5b3b058e512cb3fd1f8c36b8bfd932f86f17e3c
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-08-02 21:49:07 +0000

    caching views

--- playground/views.py
+++ playground/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.core.cache import cache
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
+from rest_framework.views import APIView
 import requests
 
-# Create your views here.
-def say_hello(request):
-    key = 'httpbin-result'
-    if cache.get(key) is None:
+class HelloView(APIView):
+    @method_decorator(cache_page(5 * 60))
+    def say_hello(self, request):
         response = requests.get('https://httpbin.org/delay/2')
         data =response.json()
-        cache.set(key, data)
-    return render(request, 'hello.html', {'name': cache.get(key)})
+        return render(request, 'hello.html', {'name': data})
