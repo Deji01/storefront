27d1eae3eda41d04d9c1c8158147b8859331bfaa
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-08-02 21:34:29 +0000

    simulating a slow API

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.core.mail import EmailMessage, BadHeaderError
-from .tasks import notify_customers
+import requests
+
 # Create your views here.
 def say_hello(request):
-    notify_customers.delay('Hello')
+    requests.get('https://httpbin.org/delay/2')
     return render(request, 'hello.html', {'name': 'Ayodeji'})
