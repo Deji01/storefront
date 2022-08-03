0fe1ab2e17f1eb60bbaf3ce5bd7b90b8b3177761
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-08-03 12:47:50 +0000

    Home Page

--- storefront/urls.py
+++ storefront/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('__debug__/', include(debug_toolbar.urls)),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path('', include('core.urls')),
 ]
 
 if settings.DEBUG:
