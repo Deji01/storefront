6a07514a7430ebf592644d0a0fa7fd8ea426ff25
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-08-03 13:10:25 +0000

    Logging

--- playground/views.py
+++ playground/views.py
@@ -4,10 +4,16 @@ from django.views.decorators.cache import cache_page
 from django.utils.decorators import method_decorator
 from rest_framework.views import APIView
 import requests
+import logging
 
+logger = logging.getLogger(__name__)
 class HelloView(APIView):
-    @method_decorator(cache_page(5 * 60))
     def say_hello(self, request):
-        response = requests.get('https://httpbin.org/delay/2')
-        data =response.json()
+        try:
+            logger.info('Calling httpbin')
+            response = requests.get('https://httpbin.org/delay/2')
+            logger.info('Received the response')
+            data =response.json()
+        except requests.ConnectionError:
+            logger.critical('httpbin is offline')
         return render(request, 'hello.html', {'name': data})
