7afd0a60cc0266a047f45efbf10d125dbc3f342b
Ayodeji Yekeen <ayodejiyou@gmail.com>
2022-08-04 13:06:17 +0000

    Prepare for deploy

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -147,7 +147,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES' : ('JWT',),
-    'ACCESS_TOKEN_LIFETIME' : timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME' : timedelta(minutes=5),
 }
 
 DJOSER = {
